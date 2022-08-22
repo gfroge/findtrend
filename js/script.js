4daf32f79b360cbaea07d2d6f1f35b4beec96bac
unknown <>
2022-08-22 23:04:56 +0300

    Pricing (not adaptive) done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-function closeTab (className) {
+function closeTab(className) {
     console.log(className);
     const tab = document.querySelector(`.${className}`);
     tab.classList.add('closed');
 }
+
+const pricingSwitch = document.querySelector('.switch__button');
+pricingSwitch.addEventListener('click', function(e){
+    pricingSwitch.classList.toggle('toggle')
+});
